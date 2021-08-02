d25699b84240b23999a232f1e5b9254b800df684
Anthony Suárez <neolight1010@gmail.com>
2021-08-02 15:11:58 -0500

    Implement find_the_best_dice()

--- combinatorics/main.py
+++ combinatorics/main.py
@@ -1,19 +1,40 @@
 from typing import List
+import itertools as it
 
 
 def count_wins(dice1: List[int], dice2: List[int]):
     assert len(dice1) == 6 and len(dice2) == 6
     dice1_wins, dice2_wins = 0, 0
-    
+
     for i in dice1:
         for j in dice2:
-            if i > j: dice1_wins += 1
-            if j > i: dice2_wins += 1
+            if i > j:
+                dice1_wins += 1
+            if j > i:
+                dice2_wins += 1
 
     return (dice1_wins, dice2_wins)
 
+
+def find_the_best_dice(dices: List[List[int]]):
+    assert all(len(dice) == 6 for dice in dices)
+
+    winners = []
+    for i, j in it.combinations(range(len(dices)), 2):
+        wins_i, wins_j = count_wins(dices[i], dices[j])
+
+        if wins_i > wins_j: winners.append(i)
+        if wins_j > wins_i: winners.append(j)
+
+    if len(set(winners)) == 1:
+        return winners[0]
+
+    return -1
+
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
--- combinatorics/test_all.py
+++ combinatorics/test_all.py
@@ -1,5 +1,30 @@
-from main import count_wins
+from main import count_wins, find_the_best_dice
+
 
 def test_count_wins():
     assert count_wins([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]) == (15, 15)
     assert count_wins([1, 1, 6, 6, 8, 8], [2, 2, 4, 4, 9, 9]) == (16, 20)
+
+
+def test_find_the_best_dice():
+    assert (
+        find_the_best_dice([[1, 1, 6, 6, 8, 8], [2, 2, 4, 4, 9, 9], [3, 3, 5, 5, 7, 7]])
+        == -1
+    )
+
+    assert (
+        find_the_best_dice([[1, 1, 2, 4, 5, 7], [1, 2, 2, 3, 4, 7], [1, 2, 3, 4, 5, 6]])
+        == 2
+    )
+
+    assert (
+        find_the_best_dice(
+            [
+                [3, 3, 3, 3, 3, 3],
+                [6, 6, 2, 2, 2, 2],
+                [4, 4, 4, 4, 0, 0],
+                [5, 5, 5, 1, 1, 1],
+            ]
+        )
+        == -1
+    )
